a528b1b4d134ad11296c1304751d5a9bf573cf4e
Pham Hoang <hoang6btdhl@gmail.com>
2024-07-01 16:00:12 +0700

    ECG_C

---
+++ ECG_classification_C/Pool.h
@@ -0,0 +1,5 @@
+void Max_Pool1D_0(float input_MaxPooling[1280], float output_MaxPooling[640]);
+void Max_Pool1D_1(float input_MaxPooling[1280], float output_MaxPooling[640]);
+void Max_Pool1D_2(float input_MaxPooling[1280], float output_MaxPooling[640]);
+void Max_Pool1D_3(float input_MaxPooling[1280], float output_MaxPooling[640]);
+void GlobalAveragePool1D(float input_GlobalAveragePool1D[640],float output_GlobalAveragePool1D[32]);
